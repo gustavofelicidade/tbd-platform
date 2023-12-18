e1cc866fbdaa832b8bce56c9224a3d97c548a8e7
Gustavo Felicidade <gustavosnik95@gmail.com>
2023-12-18 19:49:07 -0300

    Setting Django Env - Part 1

--- risk_forum/forum/views.py
+++ risk_forum/forum/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Thread, Post
+
+from django.http import HttpResponse
+
+
+
+from . import views
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class ThreadListView(ListView):
+    model = Thread
+
+
+class ThreadDetailView(DetailView):
+    model = Thread
+
+
+class CreateThreadView(CreateView):
+    model = Thread
+    fields = ['title']
+    success_url = reverse_lazy('thread_list')
+
+
+class CreatePostView(CreateView):
+    model = Post
+    fields = ['content']
+
+    def form_valid(self, form):
+        form.instance.thread_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('thread_detail', kwargs={'pk': self.kwargs['pk']})
