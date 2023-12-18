2ef9af43c73aa0a6afa8565f30bc4f42ec83872f
Gustavo Felicidade <gustavosnik95@gmail.com>
2023-12-18 19:49:07 -0300

    Setting Django Env - Part 1

--- risk_forum/risk_forum/settings.py
+++ risk_forum/risk_forum/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Init forums
+    'forum',
 ]
 
 MIDDLEWARE = [
@@ -54,8 +57,11 @@ ROOT_URLCONF = 'risk_forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS'    : 'templates',
         'APP_DIRS': True,
+
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
