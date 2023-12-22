4bd31b05517fa5c0c1de6edd0041c27409296a00
Gustavo Felicidade <gustavosnik95@gmail.com>
2023-12-21 22:59:04 -0300

    Setting Forum and Account App - part 1.2

---
+++ back_end/forum/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path('homepage.html', views.homepage),
+    path('dashboard.html', views.dashboard),
+    path('base.html', views.base),
+    path('admin-panel-page.html', views.admin_panel),
+    path('ai-risk-profile.html', views.ai_risk_profile),
+    path('finance-management-features.html', views.finance_management_features),
+    path('invitation.html', views.invitation),
+    path('to-page.html', views.to_page),
+
+]
--- back_end/forum/views.py
+++ back_end/forum/views.py
@@ -1,21 +1,50 @@
-from django.shortcuts import render
 
-# Create your views here.
+# Create your Forum views here.
 
 
-from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Thread, Post
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
-
-from . import views
 def index(request):
     return HttpResponse("Hello, world. You're at the Forum index.")
+
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+
+
+def admin_panel(request):
+    return render(request, 'admin-panel-page.html')
+
+
+def ai_risk_profile(request):
+    return render(request, 'ai-risk-profile.html')
+
+
+def finance_management_features(request):
+    return render(request, 'finance-management-features.html')
+
+
+def invitation(request):
+    return render(request, 'invitation.html')
+
+
+def to_page(request):
+    return render(request, 'to-page.html')
 
 
 class ThreadListView(ListView):
