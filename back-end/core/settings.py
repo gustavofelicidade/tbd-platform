9ed304e72177793129b0b708d783bbce2aa49d7f
Gustavo Felicidade <gustavosnik95@gmail.com>
2023-12-21 21:51:09 -0300

    Setting Forum and Account App - part 1

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Init forums
+    # After create a new APP, go to urls.py to add the path
     'forum',
+    'account'
 ]
 
 MIDDLEWARE = [
