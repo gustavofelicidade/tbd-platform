24f904998e163331a775e3610031a4e0464140a4
Lucas Inocencio <li.lucasinocencio@gmail.com>
2023-12-27 13:49:09 -0300

    start db and change lib for psycopg2

---
+++ database/client.py
@@ -0,0 +1,100 @@
+# carregando as funções em outros arquivos .py
+from Database import database as db
+import hashlib
+
+
+def make_hashes(password):
+    return hashlib.sha256(str.encode(password)).hexdigest()
+
+
+def check_hashes(password,hashed_text):
+
+    if make_hashes(password) == hashed_text:
+        return hashed_text
+
+    return False
+
+
+conn = db.init_connection()
+cur = conn.cursor()
+
+# função para inserir registros no banco de dados
+def insert(nome, nascimento, curso, dre, password, email, telefone):
+    # Incluir registros no banco de dados
+    cur.execute("""
+            INSERT into risk_forum.users (nome, idade, curso, dre, password, email, telefone) 
+            values('%s', '%s', '%s', '%s')
+    """ % (nome, nascimento, curso, dre, password, email, telefone))
+    conn.commit()
+
+# função para excluir registros no banco de dados
+def delete(id):
+    # excluir registros no  banco de dados
+    cur.execute("""
+            DELETE FROM clientes WHERE id = '%s'
+    """ % (id))
+    conn.commit()
+
+# função para alterar registros no banco de dados
+def update(nome, idade, curso, dre, id):
+    # alterar registros no banco de dados
+    cur.execute("""
+            UPDATE clientes SET (nome, idade,  curso, dre) = ('%s', '%s', '%s', '%s')  WHERE id = '%s'
+    """ % (nome, idade, curso, dre, id))
+    conn.commit()
+
+# função para selecionar todos os registros no banco de dados
+def select():
+    # selecionar registros no banco de dados
+    cur.execute("""SELECT * FROM risk_forum.users;""")
+    recset = cur.fetchall()
+    rows = []
+    for rec in recset:
+        rows.append(rec)
+    return rows
+
+# função para selecionar apenas um registros no banco de dados
+def select_id(id):
+    cur.execute("""
+            SELECT * FROM risk_forum.users WHERE id = '%s'
+    """ % (id))
+    recset = cur.fetchall()
+    rows = []
+    for rec in recset:
+        rows.append(rec)
+    return rows
+
+
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+
+def run_query(query):
+    with cur:
+        # cur.execute("SET search_path TO risk_forum")
+        cur.execute(query)
+        return cur.fetchall()
+
+
+def create_usertable():
+    cur.execute("""CREATE TABLE IF NOT EXISTS risk_forum.users(username TEXT,password TEXT)""")
+
+
+def add_userdata(username, password):
+    cur.execute("""INSERT into risk_forum.users(username,password) VALUES ('%s', '%s')""" % (username, password))
+    conn.commit()
+    print(f"added: {username, password}")
+
+def login_user(username, password):
+    cur.execute("""SELECT * FROM risk_forum.users WHERE username = '%s' AND password = '%s' """ % (username, password))
+    data = cur.fetchall()
+    return data
+
+
+def view_all_users():
+    cur.execute('SELECT * FROM risk_forum.users')
+    data = cur.fetchall()
+    return data
+
+
+
