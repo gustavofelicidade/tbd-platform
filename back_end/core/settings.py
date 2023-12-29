0677966b8ed28e0c627ec638b2f5ebcc911b6403
Lucas Inocencio <li.lucasinocencio@gmail.com>
2023-12-29 15:02:20 -0300

    Init db risk_forum

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "risk_forum",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
