f7e7a664290e6e1704ad8af8ec3c9a7e1fe5346e
lucas-inocencio <li.lucasinocencio@gmail.com>
2024-01-09 14:57:59 -0300

    init railway

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -91,7 +92,7 @@ DATABASES = {
         "PORT": 5432,  # default postgres port
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
