5964175e9cff7b62abc8b5d816d51ce7e58e698e
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-10 16:38:18 -0300

    CSRF_TRUSTED_ORIGINS = ['riskforum.up.railway.app']

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['riskforum.up.railway.app']
 
 # Application definition
 
