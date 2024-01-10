599be5de151defce609fa3e407baea108452fbc4
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-09 22:25:41 -0300

    Setting Railway APP 0.2.5.1 DEBUG FALSE Docker Branch Gustavo

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app']
