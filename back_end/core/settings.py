f81ee61e1dcc0f7fb8f2d621d1c91bc3c154e1f8
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-15 16:21:57 -0300

    Applying Design Template 0.4.6
    DEBUG False

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app/']
