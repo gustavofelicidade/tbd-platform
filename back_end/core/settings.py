d84d0725abd7ef8b0cf04b23acae8a407cfb1406
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-24 21:36:05 -0300

    Railway Deployement 0.3 CSRF verification

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app/']
