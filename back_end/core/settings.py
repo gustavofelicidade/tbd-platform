76b7e98887e99d456dd6f092ec4ab82af58e7fff
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-24 23:57:10 -0300

    Railway Deployement 0.4.3 CSRF verification

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -33,8 +33,19 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app/', 'hermandai.up.railway.app', 'https://hermandai.com']
 CSRF_COOKIE_SECURE = True  # Send CSRF cookie over HTTPS only
+
+CSRF_COOKIE_AGE = 31449600  # 1 year
+CSRF_COOKIE_DOMAIN = 'https://hermandai.up.railway.app/'
+CSRF_COOKIE_HTTPONLY = True
+
+CSRF_USE_SESSIONS = True  # Or False, depending on your requirement
+
+
+
+
+
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
