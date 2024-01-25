eada791efd6fc447838ddcc19e4767cc31d81424
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-24 21:21:48 -0300

    Railway Deployement

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = 'django-insecure-&^ur$wu(_s2$g)l1s+0#7^ygo0qdbi$)r4_kci*i=$ky*!1uj&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://hermandai.com']
-CSRF_TRUSTED_ORIGINS = ['https://hermandai.com']
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://hermandai.up.railway.app/']
 
 # settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
