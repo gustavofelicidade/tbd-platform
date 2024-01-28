ef6fd01517c915d3715e6f99c5f97ded302dc318
Gustavo Felicidade <gustavosnik95@gmail.com>
2024-01-28 17:41:59 -0300

    Adding Django Rest Framework 0.5.5 Integrity Error in production

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -70,7 +70,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Use environment variable
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use environment variable
 
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
